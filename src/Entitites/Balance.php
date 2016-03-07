7491138085301f538c1b2fd2ff52f6f2de23651a
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-07 13:04:58 +0000

    Updated docblock for balance entity

--- src/Entitites/Balance.php
+++ src/Entitites/Balance.php
@@ -2,6 +2,15 @@
 
 namespace Edcs\Mondo\Entitites;
 
+/**
+ * An object descibing a mondo bank account.
+ *
+ * @link https://getmondo.co.uk/docs/#balance
+ *
+ * @method int getBalance() Getter for the account balance - returns the balance in pence.
+ * @method int getCurrency() Getter for the account ISO currency code.
+ * @method int getSpendToday() Getter for the spend today amount - returns the value in pence.
+ */
 class Balance extends Entity
 {
     //
