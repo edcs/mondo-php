09905739fae888730642237d96718ede8d588bad
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-07 11:37:10 +0000

    Added an account entity

---
+++ src/Entitites/Accounts/Account.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Edcs\Mondo\Entitites\Accounts;
+
+use Edcs\Mondo\Entitites\Entity;
+
+/**
+ * An object descibing a mondo bank account.
+ *
+ * @link https://getmondo.co.uk/docs/#accounts
+ *
+ * @method string getId() Getter for the id of the account.
+ * @method string getDescription() Getter for the description for the account.
+ * @method string getAccountNumber() Getter for the account number.
+ * @method string getSortCode() Getter for the sort code.
+ * @method string getCreated() Getter for the date when the account was created.
+ */
+class Account extends Entity
+{
+    //
+}
