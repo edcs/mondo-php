f56605c0e40db78e35d7c97d240b4d7d2f1a062c
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-07 17:31:19 +0000

    Added transaction entity

---
+++ src/Entitites/Transaction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Edcs\Mondo\Entitites;
+
+/**
+ * An object descibing a transaction.
+ *
+ * @link https://getmondo.co.uk/docs/#transactions
+ *
+ * @method string () Getter for the.
+ */
+class Transaction extends Entity
+{
+    //
+}
