b2fb613bd7098a46f5b0f95c2472cbec3d18d633
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-07 13:01:52 +0000

    Added a money formatter

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -18,4 +18,19 @@ class Str
             return '_' . strtolower($string[1]);
         }, $str);
     }
+
+    /**
+     * Converts the supplied value from pence into it's decimalised format.
+     *
+     * @param int $pence
+     * @param string $locale
+     * @param string $format
+     * @return string
+     */
+    public static function money($pence, $locale = 'en_GB.UTF-8', $format = '%n')
+    {
+        $value = $pence / 100;
+        setlocale(LC_MONETARY, $locale);
+        return money_format($format, $value);
+    }
 }
