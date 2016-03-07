00361663914a925e6f9ec3d3b60371b2f56a249a
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-07 17:42:27 +0000

    Updated docblocks

--- src/Entitites/Balance.php
+++ src/Entitites/Balance.php
@@ -8,7 +8,7 @@ namespace Edcs\Mondo\Entitites;
  * @link https://getmondo.co.uk/docs/#balance
  *
  * @method int getBalance() Getter for the account balance - returns the balance in pence.
- * @method int getCurrency() Getter for the account ISO currency code.
+ * @method string getCurrency() Getter for the account ISO currency code.
  * @method int getSpendToday() Getter for the spend today amount - returns the value in pence.
  */
 class Balance extends Entity
--- src/Entitites/Transaction.php
+++ src/Entitites/Transaction.php
@@ -7,9 +7,32 @@ namespace Edcs\Mondo\Entitites;
  *
  * @link https://getmondo.co.uk/docs/#transactions
  *
- * @method string () Getter for the.
+ * @method int getAccountBalance() Getter for the the account balance after the transaction - returns the balance in pence.
+ * @method int getAmount() Getter for the the transaction amount - returns the balance in pence.
+ * @method string getCreated() Getter for the transaction created date.
+ * @method string getCurrency() Getter for the transaction ISO currency code.
+ * @method string getDescription() Getter for the transaction description.
+ * @method string getId() Getter for the transaction id.
+ * @method array getMerchant() Getter for the merchant information array.
+ * @method array getMetadata() Getter for any metadata associated with this transaction.
+ * @method array getNotes() Getter for any notes associated with this transaction.
+ * @method boolean getIsLoad() Getter for the is load property of the transaction.
+ * @method boolean|string getSettled() Getter for the settled property - returns boolean or the dat when the transaction was settled.
  */
 class Transaction extends Entity
 {
-    //
+    /**
+     * Getter for the decline reason property - needs to be explicitly defined because this property is not always
+     * present in the response object.
+     *
+     * @return string|null
+     */
+    public function getDeclineReason()
+    {
+        if ($this->offsetExists('decline_reason')) {
+            return $this->offsetGet('decline_reason');
+        }
+
+        return null;
+    }
 }
